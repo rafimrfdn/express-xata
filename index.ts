a06ad7ebdfa116cdcc10c8730cffee885bb7fab3
rafimrfdn <uphy.mks@gmail.com>
2024-05-04 17:52:50 +0800

    init denodeploy new

--- index.ts
+++ index.ts
@@ -2,12 +2,8 @@ import express from 'npm:express@4.18.2';
 import path from 'npm:path@0.12.7'; // To resolve file paths
 import bodyParser from "npm:body-parser@1.20.2";
 import userRoutes from './src/routes/userRoutes.js';
+import ejs from 'npm:ejs@3.1.10';
 
-
-const { cwd, stdout, copy } = Deno;
-import ejs from "https://deno.land/x/dejs@0.10.3/mod.ts";
-
-// import * as mod from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 const env = await load();
@@ -35,9 +31,6 @@ app.get('*', (req, res) => {
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./src/views'));
-
-// const output = await renderFile(`${cwd()}/src/views/index.ejs`);
-// await copy(output, stdout);
 
 // Error handling middleware (optional)
 app.use((err, req, res, next) => {
